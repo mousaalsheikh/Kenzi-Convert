51f49910ee90a4664e93fe6cd64c1482df8c3bd1
Musa Sheikh <musa.sheikh@outlook.com>
2023-08-02 11:11:22 -0700

    Add project files.

---
+++ Kenzi Convert/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Kenzi_Convert
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboFromFile = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cboToFile = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.btnChooseFolder = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnConvert = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(301, 67);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(82, 350);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(396, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Copyrights © 2023 Kinzi Workspace LLC, All Rights Reserved";
+            // 
+            // cboFromFile
+            // 
+            this.cboFromFile.BackColor = System.Drawing.SystemColors.Menu;
+            this.cboFromFile.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboFromFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboFromFile.FormattingEnabled = true;
+            this.cboFromFile.Items.AddRange(new object[] {
+            "image/*",
+            "png",
+            "jpg",
+            "gif"});
+            this.cboFromFile.Location = new System.Drawing.Point(21, 218);
+            this.cboFromFile.Name = "cboFromFile";
+            this.cboFromFile.Size = new System.Drawing.Size(240, 26);
+            this.cboFromFile.TabIndex = 2;
+            this.cboFromFile.SelectedIndexChanged += new System.EventHandler(this.cboFromFile_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(18, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "From Extension";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(301, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "To Extension";
+            // 
+            // cboToFile
+            // 
+            this.cboToFile.BackColor = System.Drawing.SystemColors.Menu;
+            this.cboToFile.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboToFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboToFile.FormattingEnabled = true;
+            this.cboToFile.Items.AddRange(new object[] {
+            "webp",
+            "png",
+            "jpg"});
+            this.cboToFile.Location = new System.Drawing.Point(304, 218);
+            this.cboToFile.Name = "cboToFile";
+            this.cboToFile.Size = new System.Drawing.Size(240, 26);
+            this.cboToFile.TabIndex = 4;
+            this.cboToFile.SelectedIndexChanged += new System.EventHandler(this.cboToFile_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(18, 134);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Folder";
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFolder.Location = new System.Drawing.Point(21, 157);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.Size = new System.Drawing.Size(409, 21);
+            this.txtFolder.TabIndex = 7;
+            // 
+            // btnChooseFolder
+            // 
+            this.btnChooseFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChooseFolder.Location = new System.Drawing.Point(436, 153);
+            this.btnChooseFolder.Name = "btnChooseFolder";
+            this.btnChooseFolder.Size = new System.Drawing.Size(108, 29);
+            this.btnChooseFolder.TabIndex = 8;
+            this.btnChooseFolder.Text = "Select Folder";
+            this.btnChooseFolder.UseVisualStyleBackColor = true;
+            this.btnChooseFolder.Click += new System.EventHandler(this.btnChooseFolder_Click);
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.BackColor = System.Drawing.Color.Navy;
+            this.btnConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvert.ForeColor = System.Drawing.Color.White;
+            this.btnConvert.Location = new System.Drawing.Point(424, 275);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(120, 38);
+            this.btnConvert.TabIndex = 9;
+            this.btnConvert.Text = "CONVERT";
+            this.btnConvert.UseVisualStyleBackColor = false;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(557, 374);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.btnChooseFolder);
+            this.Controls.Add(this.txtFolder);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cboToFile);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cboFromFile);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kenzi Convert";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboFromFile;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cboToFile;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.Button btnChooseFolder;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnConvert;
+    }
+}
+
---
+++ Kenzi Convert/Form1.cs
@@ -0,0 +1,189 @@
+﻿using ImageProcessor;
+using ImageProcessor.Plugins.WebP.Imaging.Formats;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kenzi_Convert
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChooseFolder_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtFolder.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtFolder.Text))
+            {
+                MessageBox.Show("Please select folder");
+            }
+            else if (string.IsNullOrEmpty(cboFromFile.Text))
+            {
+                MessageBox.Show("Please select extension");
+            }
+            else if (string.IsNullOrEmpty(cboToFile.Text))
+            {
+                MessageBox.Show("Please select extension");
+            }
+            else if (cboFromFile.Text == cboToFile.Text)
+            {
+                MessageBox.Show("Please select different extension to convert");
+            }
+            else
+            {
+                btnChooseFolder.Enabled = false;
+                btnConvert.Enabled = false;
+                int count = 0;
+                int failed = 0;
+                int total = 0;
+
+                foreach (string file in Directory.GetFiles(txtFolder.Text))
+                {
+                    bool canConvert = false;
+                    string ext = Path.GetExtension(file).ToLower();
+                    if (cboFromFile.Text == "image/*")
+                    {
+                        if (ext == ".png" || ext == ".jpg" || ext == ".jpeg" || ext == ".gif")
+                        {
+                            total++;
+                            canConvert = true;
+                        }
+                    }
+                    else
+                    {
+                        if (cboFromFile.Text == "jpg")
+                        {
+                            if (ext == ".jpg" || ext == ".jpeg")
+                            {
+                                total++;
+                                canConvert = true;
+                            }
+                        }
+                        else
+                        {
+                            if (ext == "." + cboFromFile.Text)
+                            {
+                                total++;
+                                canConvert = true;
+                            }
+                        }
+                    }
+
+
+                    if (canConvert)
+                    {
+                        string name = Path.GetFileName(file);
+                        string fileId = Path.GetFileNameWithoutExtension(file);
+                        string path = Path.GetDirectoryName(file);
+                        string fullFilePath = path + @"/" + name;
+                        if (cboToFile.Text == "png")
+                        {
+                            try
+                            {
+                                using (Bitmap bitmap = new Bitmap(fullFilePath))
+                                {
+                                    using (Bitmap newImage = new Bitmap(bitmap.Width, bitmap.Height))
+                                    {
+                                        using (Graphics graphics = Graphics.FromImage(newImage))
+                                        {
+                                            graphics.Clear(Color.White);
+                                            graphics.DrawImage(bitmap, Point.Empty);
+                                            newImage.Save(path + @"/" + fileId + ".png", ImageFormat.Png);
+                                        }
+                                    }
+                                }
+                                count++;
+                            }
+                            catch
+                            {
+                                failed++;
+                            }
+                        }
+                        else if (cboToFile.Text == "jpg")
+                        {
+                            try
+                            {
+                                using (Bitmap bitmap = new Bitmap(fullFilePath))
+                                {
+                                    using (Bitmap newImage = new Bitmap(bitmap.Width, bitmap.Height))
+                                    {
+                                        using (Graphics graphics = Graphics.FromImage(newImage))
+                                        {
+                                            graphics.Clear(Color.White);
+                                            graphics.DrawImage(bitmap, Point.Empty);
+                                            newImage.Save(path + @"/" + fileId + ".jpeg", ImageFormat.Jpeg);
+                                        }
+                                    }
+                                }
+                                count++;
+                            }
+                            catch
+                            {
+                                failed++;
+                            }
+                        }
+                        else if (cboToFile.Text == "webp")
+                        {
+                            try
+                            {
+                                string webPImagePath = path + @"/" + fileId + ".webp";
+                                using (var webPFileStream = new FileStream(webPImagePath, FileMode.Create))
+                                using (var imageFactory = new ImageFactory(preserveExifData: false))
+                                {
+                                    using (Bitmap bitmap = new Bitmap(fullFilePath))
+                                    {
+                                        imageFactory.Load(bitmap)
+                                                          .Format(new WebPFormat())
+                                                          .Quality(90)
+                                                          .Save(webPFileStream);
+                                    }
+                                }
+                                count++;
+                            }
+                            catch
+                            {
+                                failed++;
+                            }
+                        }
+                    }
+                }
+                MessageBox.Show("Total Images: " + total.ToString() + "\n\n" + "Successfull: " + count.ToString() + " - Failed: " + failed.ToString());
+                btnChooseFolder.Enabled = true;
+                btnConvert.Enabled = true;
+            }
+        }
+
+        private void cboFromFile_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cboToFile_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
